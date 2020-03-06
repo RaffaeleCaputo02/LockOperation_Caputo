f2ea6ea3564ab5c334ddef11f7b25ae49ab28e85
rcaputo95 <rcaputo95@gmail.com>
2020-03-06 12:50:57 +0100

    esercizio Lock operation finito e funzionante

--- LockOperation_Caputo/Program.cs
+++ LockOperation_Caputo/Program.cs
@@ -2,15 +2,59 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace LockOperation_Caputo
 {
     class Program
     {
+        static double _val1 = 1, _val2 = 2;
+
+        static object padlock = new object();
+        static void Go()
+        {
+            lock (padlock)
+            { 
+                if (_val2 != 0)
+                {
+                    Console.WriteLine(_val1 / _val2);
+                    _val2 = 0;
+                }
+                else
+                    Console.WriteLine("No");
+            }
+        }
+
+        static void Go2()
+        {
+            Monitor.Enter(padlock);
+            try
+            {
+                Console.WriteLine(_val1 / _val2);
+                _val2 = 0;
+            }
+            catch
+            {
+                Console.WriteLine("operazione non eseguibile");
+            }
+            finally
+            {
+                Monitor.Exit(padlock);
+            }
+
+        }
         static void Main(string[] args)
         {
+            //Task.Factory.StartNew(Go);
+            //Task.Factory.StartNew(Go);
+            //Task.Factory.StartNew(Go);
 
+            Task.Factory.StartNew(Go2);
+            Task.Factory.StartNew(Go2);
+            Task.Factory.StartNew(Go2);
+
+            Console.ReadLine();
         }
     }
 }
